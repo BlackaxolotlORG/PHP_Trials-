0b596ce6efe07d333b64e223371eca34d2f50655
BlackaxolotlORG <diegoloparasot@gmail.com>
2023-09-12 14:26:18 -0600

    Create Site.PHP
    creating environment were i will work

---
+++ Site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"> 
+    /* Unicode Transformation Format 8-bit */
+    <title>
+        "Php website one"
+    </title>
+</head>
+<body>
+    <?php
+    echo "hello world"
+    ?>
+</body>
+</html>
