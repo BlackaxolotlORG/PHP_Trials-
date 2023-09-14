ee6253edf81f526db85a88fc15564a826dc4c541
BlackaxolotlORG <diegoloparasot@gmail.com>
2023-09-14 15:31:44 -0600

    Variables
    make a story to test variables in php

--- Site.php
+++ Site.php
@@ -9,7 +9,18 @@
 </head>
 <body>
     <?php
-    echo "hello world"
+    echo "<h1>hello world</h1>";
+    echo "<hr>";
+    echo"<p>Welcome to my new website</p>";
+    ?>
+
+<?php
+    echo "<h1>This is a story to see variables in PHP</h1>";
+    echo "<hr>";
+    echo"There once was a man named George<br>";
+    echo"He lived in a hill<br>";
+    echo"He was 70 years old and was never ill<br>";
+    echo"So he left the hill to live his years elsewhere<br>";
     ?>
 </body>
 </html>
