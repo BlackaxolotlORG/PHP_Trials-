320dce030dea8c66f7e2046cd28484eb4bc3b494
BlackaxolotlORG <diegoloparasot@gmail.com>
2023-09-14 16:07:53 -0600

    Learning more php
    Learning Php in strings and variables

--- Site.php
+++ Site.php
@@ -15,12 +15,52 @@
     ?>
 
 <?php
+    $characterName = "John";
+    $characterAge = 35;
+    $characterLocation = "Ocean";
     echo "<h1>This is a story to see variables in PHP</h1>";
     echo "<hr>";
-    echo"There once was a man named George<br>";
-    echo"He lived in a hill<br>";
-    echo"He was 70 years old and was never ill<br>";
-    echo"So he left the hill to live his years elsewhere<br>";
+    echo"There once was a man named  $characterName <br>";
+    echo"He lived in a $characterLocation<br>";
+
+    $characterLocation = "City";
+    echo"He was $characterAge years old and was never ill<br>";
+    echo"So he left the $characterLocation to live his years elsewhere<br>";
     ?>
+
+<?php
+    echo "<hr>";
+    $phrase = "Hello";
+    $integerAge = 45;
+    $floatAge = 45.66;
+    $booleanisMale = false;
+    $booleanRoisMale =  null;
+    echo $floatAge;
+?>
+
+<?php
+    echo "<hr>";
+    echo strtolower($phrase);
+    echo "<br>";
+    echo strtoupper($phrase);
+    echo "<br>";
+    echo strlen($phrase);
+    echo "<br>";
+    echo $phrase[0];
+    echo "<br>";
+    echo strtoupper($phrase[0]);
+    echo strtolower($phrase[2]);
+    echo "<br>";
+    $phrase2 = "Giraffe academy";
+    echo $phrase2[0] = "B";
+    echo "<br>";
+    echo $phrase2;
+?>
+<?php
+     echo "<hr>";
+     $phrase3 = "Giraffe academy";
+     echo str_replace("Giraffe", "Panda", $phrase3);
+
+?>
 </body>
 </html>
