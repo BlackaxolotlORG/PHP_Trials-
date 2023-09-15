ecda9f42a33b161935d6127da7d87613de794636
BlackaxolotlORG <diegoloparasot@gmail.com>
2023-09-15 17:56:44 -0600

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"> 
+
+    <title>
+        Php website Calculator
+    </title>
+</head>
+<body>
+
+<form action="calculator.php" method="get">
+    <input type="number" name="num1">
+    <br>
+    <input type="number" name="num2">
+    <input type="submit">
+</form>
+    <?php
+
+?>
+
+</body>
+</html>
